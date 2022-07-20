6fa60dc3660393a615621d3870260019e961b71b
znmbrk <zain.mobarik@challoners.org>
2022-07-20 05:15:09 +0100

    Nav Bar and Home Page
    Styling nav bar and adding media queries as well as a button for smaller screens to open up nav bar.
    
    Styling home page!

--- static/js/nav.js
+++ static/js/nav.js
@@ -10,29 +10,3 @@ navLinks.forEach(link => {
         document.body.classList.remove("nav-open");
     })
 })
-
-/*
-$(document).ready(function() {
-    $(".explode").click(function() {
-        $(".logo__img").explode({
-            omitLastLine: false,
-            radius: 80,
-            minRadius: 20,
-            release: true,
-            fadeTime:300,
-            recycle:true,
-            recycleDelay:500,
-            fill:true,
-            explodeTime:300,
-            maxAngle:360,
-            gravity:0,
-            round:false,
-            groundDistance:400,
-            ignoreCompelete:false,
-            land:true,
-            checkOutBound,
-            finish
-        });
-    });
-});
-*/
